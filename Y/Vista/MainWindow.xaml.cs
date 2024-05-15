f5893962bf4d98aa3e28192cd8854c59016aac05
Pozok05 <a2308766@institutmontilivi.cat>
2024-05-15 14:18:27 +0200

    Canvia amb branca oriol

--- Y/Vista/MainWindow.xaml.cs
+++ Y/Vista/MainWindow.xaml.cs
@@ -1,15 +1,4 @@
-﻿using System.Text;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
-using Y;
-using Y.Model;
+﻿using System.Windows;
 
 namespace Y
 {
@@ -21,7 +10,6 @@ namespace Y
         public MainWindow()
         {
             InitializeComponent();
-            Usuari usuari = new Usuari();
         }
     }
 }
