4072cb9e31c284fcd8279ee858f15f33d689fd66
Pozok05 <a2308766@institutmontilivi.cat>
2024-05-15 18:14:34 +0200

    Carpetes+vistesOriol

---
+++ Y/Model/Publicacio.cs
@@ -0,0 +1,20 @@
+﻿using Y;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Y.Model
+{
+    public partial class Publicacio
+    {
+        public int publicacio_id { get; set; }
+        public int user_id { get; set; }
+        public int data_p { get; set; }
+        public int titol { get; set; }
+        public int contingut { get; set; }
+        public int tags { get; set; }
+
+    }
+}
